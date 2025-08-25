83d267d7e3669e1d854d4c10408e8aad5cff1d28
DCtime <daniel.chu916@gmail.com>
2025-08-25 22:42:26 +0800

    BetterAdvancement 可以加字且是灰色

--- src/main/java/net/github/dctime/mixin/BetterAdvancementWidgetMixin.java
+++ src/main/java/net/github/dctime/mixin/BetterAdvancementWidgetMixin.java
@@ -4,6 +4,7 @@ import betteradvancements.common.gui.BetterAdvancementTab;
 import betteradvancements.common.gui.BetterAdvancementWidget;
 import betteradvancements.common.gui.BetterAdvancementsScreen;
 import betteradvancements.common.util.CriterionGrid;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.github.dctime.libs.Translator;
 import net.minecraft.advancements.AdvancementNode;
 import net.minecraft.advancements.AdvancementProgress;
@@ -25,6 +26,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
@@ -59,8 +61,18 @@ public abstract class BetterAdvancementWidgetMixin {
     @Final
     private DisplayInfo displayInfo;
 
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+
+    @Shadow
+    protected int y;
+    @Shadow
+    protected int x;
+
     private List<FormattedCharSequence> tempDescription;
     private String tempTitle;
+    private String translatedTitle;
 
     @Shadow
     protected abstract List<FormattedText> findOptimalLines(Component line, int width);
@@ -72,7 +84,8 @@ public abstract class BetterAdvancementWidgetMixin {
         tempTitle = this.title;
         tempDescription = description.stream().toList();
 
-        this.title = this.title + " [MODDED]";
+        this.translatedTitle = " " + "This is translated title";
+        this.title = this.title + this.translatedTitle;
 
         // resize UI
         int k = 0;
@@ -98,10 +111,36 @@ public abstract class BetterAdvancementWidgetMixin {
         }
 
         this.width = maxWidth + 8;
+        this.title = tempTitle;
+    }
+
+    @Inject(method = "drawHover", at = @At(value = "INVOKE", target = "drawString", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void drawHoverLeftNoS(GuiGraphics guiGraphics, int scrollX, int scrollY, float fade, int left, int top, CallbackInfo ci, @Local(name = "drawX") int drawX) {
+        guiGraphics.drawString(this.minecraft.font, this.translatedTitle, drawX + 5 + this.minecraft.font.width(this.title), scrollY + this.y + 9, Translator.translatedStyle.getColor().getValue());
+        System.out.println("drawString 0 called");
+    }
+
+    @Inject(method = "drawHover", at = @At(value = "INVOKE", target = "drawString", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void drawHoverLeftS(GuiGraphics guiGraphics, int scrollX, int scrollY, float fade, int left, int top, CallbackInfo ci, @Local(name = "s") String s, @Local(name = "i") int i) {
+//        guiGraphics.drawString(this.minecraft.font, this.translatedTitle, scrollX + this.x - i + this.minecraft.font.width(s), scrollY + this.y + 9, Translator.translatedStyle.getColor().getValue());
+        System.out.println("drawString 1 called");
+    }
+
+    @Inject(method = "drawHover", at = @At(value = "INVOKE", target = "drawString", ordinal = 2))
+    public void drawHoverRightNoS(GuiGraphics guiGraphics, int scrollX, int scrollY, float fade, int left, int top, CallbackInfo ci) {
+        guiGraphics.drawString(this.minecraft.font, this.translatedTitle, scrollX + this.x + 32 + this.minecraft.font.width(this.title), scrollY + this.y + 9, Translator.translatedStyle.getColor().getValue());
+        System.out.println("drawString 2 called");
+    }
+
+    @Inject(method = "drawHover", at = @At(value = "INVOKE", target = "drawString", ordinal = 3), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void drawHoverRightS(GuiGraphics guiGraphics, int scrollX, int scrollY, float fade, int left, int top, CallbackInfo ci, @Local(name = "s") String s, @Local(name = "i") int i) {
+//        guiGraphics.drawString(this.minecraft.font, this.translatedTitle, scrollX + this.x + this.width - i - 5 + this.minecraft.font.width(s), scrollY + this.y + 9, Translator.translatedStyle.getColor().getValue());
+        System.out.println("drawString 3 called");
     }
 
     @Inject(method = "drawHover", at = @At(value = "RETURN"))
     public void endDrawHover(GuiGraphics guiGraphics, int scrollX, int scrollY, float fade, int left, int top, CallbackInfo ci) {
+        this.translatedTitle = "";
         this.title = tempTitle;
         this.description = tempDescription.stream().toList();
     }
